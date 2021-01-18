8eb9be31d0cd07c2ad0e8d7e75a12aebbbeafed3
Javier <javierperez@javiers-mbp.home>
2021-01-17 19:57:08 -0500

    creating app directories

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require('express');
+const logger = require('morgan');
+let app = express();
+let PORT = PORT.env || 3000;
+let mongoose = require('mongoose');
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true }).then(
+    app.listen(PORT).then(()=>{
+        console.log(`listening to port ${PORT}`)
+    }).catch((err)=>{
+        console.log(err);
+    })
+)
+
