509348ebd2b4c58e6755d6c8e60b594bf90f3cdd
Javier <javierperez@javiers-mbp.home>
2021-01-21 18:38:43 -0500

    routes created for totalduration

--- models/workout.js
+++ models/workout.js
@@ -2,34 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workOutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
-        default: Date.now
+        default: Date.now()
     },
     exercises: [{
         type: {
             type: String,
-            required: true,
         },
         name: {
             type: String,
-            required: true,
         },
         duration: {
             type: Number,
-            required: true,
         },
         weight: {
             type: Number,
-            required: true,
         },
         reps: {
             type: Number,
-        required:true,
         },
         sets: {
             type: Number,
-            required: true,
+        },
+        distance:{
+            type:Number
         }
     }]
 
--- routes/workOutApi.js
+++ routes/workOutApi.js
@@ -1,28 +1,66 @@
 const router = require('express').Router();
-const { Workout } = require('../models');
+const {
+    Workout
+} = require('../models');
 
 
-router.get('/workouts',(req,res)=>{
-   
-        Workout.find({}).then((workOutData )=>{
-            res.json(workOutData);
-        }).catch((err)=>{
-throw err;
-        })
+router.get('/workouts', (req, res) => {
+
+    Workout.aggregate([{
+
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }]).then((workOutData) => {
+        res.json(workOutData);
+    }).catch((err) => {
+        throw err;
+    })
 });
 
-router.post('/workouts',({body},res)=>{
-   Workout.create(body).then((workOutData)=>{
-       res.json(workOutData);
-   }).catch((err)=>{
-       throw err;
-   })
+router.post('/workouts', ({
+    body
+}, res) => {
+    Workout.create(body).then((workOutData) => {
+        res.json(workOutData);
+    }).catch((err) => {
+        throw err;
+    })
 });
 
-router.put('/workout/:id',(req,res)=>{
-Workout.update({_id:req.params.id},{ $set:{ exercices: req.body }}).then((response)=>{
-    res.send(response)
+router.put('/workouts/:id', (req, res) => {
+    Workout.update({
+        _id: req.params.id
+    }, {
+        $push: {
+            exercises: {
+                $each: [req.body],
+                $position: 0
+            }
+        }
+    }).then((response) => {
+        res.send(response)
+    }).catch((err) => {
+        res.status(400).send(err.message);
+    })
+});
+
+router.get('/workouts/range',(req,res)=>{
+    Workout.aggregate([{
+
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }]).limit(10).then((workOutData) => {
+        res.json(workOutData);
+    }).catch((err) => {
+        throw err;
+    })
 })
-})
- 
+
+
 module.exports = router;
